e47aa0eb267bcb576058815589d5919a0220f3f7
Ankur Gupta <rukna1000@gmail.com>
2017-05-22 20:58:25 -0400

    Added skeleton structure to index.php

--- Website/index.php
+++ Website/index.php
@@ -3,6 +3,22 @@
     <title>Pingree Greenhouse</title>
 </head>
 <body>
+    <div id="container">
+        <div id="banner">
+            <h1>Pingree Greenhouse Control Panel</h1>
+        </div>
 
+        <div id="water_tank_status">
+        </div>
+
+        <div id="quick_water">
+        </div>
+
+        <div id="settings">
+        </div>
+
+        <div id="schedule">
+        </div>
+    </div>
 </body>
 </html>
