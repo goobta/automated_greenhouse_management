4f27b5f1b457d743dce9d8a0bd59662fdd0c9523
Ankur Gupta <rukna1000@gmail.com>
2017-05-22 20:38:08 -0400

    Added logging capabilities

--- Website/php/Utility/log.php
+++ Website/php/Utility/log.php
@@ -3,3 +3,23 @@
  * www.github.com/agupta231
  * Date: 5/22/2017
  */
+
+class Log {
+    public static $logFile;
+
+    public static function init() {
+        Log::$logFile = fopen('/var/log/ping_greenhouse/log' . date("-Y-m-d") .  ".txt", "a") or die("File cannot be opened");
+    }
+
+    public static function writeLog($string, $verbose = false) {
+        if($verbose) {
+            echo $string;
+        }
+
+        fwrite(Log::$logFile, $string);
+    }
+
+    public static function closeLog() {
+        fclose(Log::$logFile);
+    }
+}
