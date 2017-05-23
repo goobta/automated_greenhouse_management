87a2ca9be282c9ab3c1acb9ffc21f46d94615ad5
Ankur Gupta <rukna1000@gmail.com>
2017-05-22 20:55:04 -0400

    Modifed files and updated filestructure

---
+++ Website/php/Blocks/schedule.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * www.github.com/agupta231
+ * Date: 5/22/17
+ */
+
+class Schedule {
+    public function generateSchedule() {
+
+    }
+    public static function addEvent() {
+
+    }
+}
