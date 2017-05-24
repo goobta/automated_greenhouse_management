585f3c9415d610efb7f830fb5a51654dfdbd1a82
Ankur Gupta <rukna1000@gmail.com>
2017-05-24 08:24:23 -0400

    Got the ulgy version of the schedule working: Finna add some JS and then work on the DB type shit

--- Website/php/Blocks/schedule.php
+++ Website/php/Blocks/schedule.php
@@ -8,56 +8,34 @@ include "php/Utility/config.php";
 
 class Schedule {
     public function generateSchedule() {
-        echo "<table id='schedule' width=100%>";
+//        $connection = new mysqli(Config::$dbHost, Config::$dbUsername, Config::$dbPass, Config::$dbName);
+//
+//        if($connection->connect_error) {
+//            die("Connection Failed: " . $connection->connect_error);
+//        }
 
-        echo "
-            <tr>
-                <td></td>
-                <td>Sun</td>
-                <td>Mon</td>
-                <td>Tue</td>
-                <td>Wed</td>
-                <td>Thu</td>
-                <td>Fri</td>
-                <td>Sat</td>
-            </tr>
-        ";
+        echo "<table>";
 
-        for ($i = 0; $i < 24; $i++) {
-            echo "
-                <tr>
-                    <td>" . $i . ":00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>";
+        for($i = 1; $i <= Config::$bedCount; $i++) {
+            echo "<tr class='header'><th>Bed " . $i . " <span>Show</span></th></tr>";
+
+//            $query = "SELECT Id, Weekday, StartTime, Duration, Pressure FROM Events WHERE Bed LIKE " . $i;
+//            $result = $connection->query($query);
+//
+//            if($result->num_rows > 0) {
+//                while ($row = $result->fetch_assoc()) {
+//
+//                }
+//            }
+//            else {
+                echo "<tr><td>No Watering Schedule Set</td></tr>";
+//            }
         }
+
         echo "</table>";
-
-        $connection = new mysqli(Config::$dbHost, Config::$dbUsername, Config::$dbPass, Config::$dbName);
-
-        if($connection->connect_error) {
-            die("Connection Failed: " . $connection->connect_error);
-        }
-
-        $query = "SELECT id, bed, day, time, duration, pressure FROM Events";
-        $result = $connection->query($query);
-
-        if($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-
-            }
-        }
 
     }
     public static function addEvent() {
-
-    }
-    private function createEventUI() {
 
     }
 }
