eb24a15f1368edd1ae9643175fb5e58648c4dfc5
Ankur Gupta <rukna1000@gmail.com>
2017-05-25 08:24:26 -0400

    Added delete event page

---
+++ Website/php/Blocks/Schedule/deleteEvent.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * www.github.com/agupta231
+ * Date: 5/25/17
+ */
--- Website/php/Blocks/Schedule/updateEvent.php
+++ Website/php/Blocks/Schedule/updateEvent.php
@@ -1,5 +1,17 @@
-<?php
-/**
- * www.github.com/agupta231
- * Date: 5/24/17
- */
+<html>
+<head>
+    <title>Edit Watering Event</title>
+</head>
+<body>
+    <?php
+        $id = $_GET['id'];
+        $weekday = $_GET['wd'];
+        $startTime = $_GET['st'];
+        $duration = $_GET['dur'];
+    ?>
+
+    <a href="index.php"><button>Go Back</button></a>
+    <a href="deleteEvent.php?id=" <?php echo $id ?>><button>Delete Event</button></a>
+
+</body>
+</html>
