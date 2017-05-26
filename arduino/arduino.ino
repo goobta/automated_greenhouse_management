a536290952220225456b9fa942b9598370868314
Ankur Gupta <rukna1000@gmail.com>
2017-05-26 11:48:40 -0400

    Began working on the arduino code
    Added basic pin numbers for the beds in the solenoids: going to start
    to make it work with serial inputs to make it work with the python
    backend that I’m going to make on the Pi Server

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+// PingreenHouse Arduino Management Code
+// www.github.com/agupta231
+// May 26, 2017
 
+const int bedPins[] = {
+                        2, //Bed 1
+                        3, //Bed 2
+                        4, //Bed 3
+                        5, //Bed 4
+                        6, //Bed 5
+                        7  //Bed 6
+                      };
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
