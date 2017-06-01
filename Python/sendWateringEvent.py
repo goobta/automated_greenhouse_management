5dcf234b6146d4b98aab79fc0d88bf3c63224d35
Ankur Gupta <rukna1000@gmail.com>
2017-06-01 07:16:52 -0400

    Added logging to the python file

--- Python/sendWateringEvent.py
+++ Python/sendWateringEvent.py
@@ -1,9 +1,16 @@
 import serial
 import sys
+import time
 
 ser = serial.Serial('/dev/cu.usbmodem1421', 115200)
 
 bed_number = sys.argv[0]
 duration = sys.argv[1]
 
+log_file = fopen("log_file_" + str(time.strftime("%H:%M:%S")) + ".txt", "a+")
+
+log_file.write("water|" + str(bed_number) + "|" + str(duration) + "|")
+
 ser.write("water|" + str(bed_number) + "|" + str(duration) + "|")
+
+log_file.close()
