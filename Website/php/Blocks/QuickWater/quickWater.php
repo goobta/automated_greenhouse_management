98d1bfa005ad617edeef82b82cfa92269aeb2e31
Ankur Gupta <rukna1000@gmail.com>
2017-06-03 10:23:32 -0400

    Had to specify full path for python

--- Website/php/Blocks/QuickWater/quickWater.php
+++ Website/php/Blocks/QuickWater/quickWater.php
@@ -14,7 +14,7 @@
         $duration = Schedule::minutesToSeconds($minutes, $seconds);
 
         foreach($beds as $bed) {
-            exec("python /home/pi/Documents/automated_greenhouse_management/Python/Water.py " . $bed . " " . $duration . " > /dev/null 2>/dev/null &");
+            exec("/usr/bin/python /home/pi/Documents/automated_greenhouse_management/Python/Water.py " . $bed . " " . $duration . " > /dev/null 2>/dev/null &");
 
             sleep(1);
         }
