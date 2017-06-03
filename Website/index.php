4a4aa77ef846e846627d99a94260c39ae4fc4308
Ankur Gupta <rukna1000@gmail.com>
2017-06-03 08:01:56 -0400

    Downloaded JQuery b/c I forgot that the Pi won't have internet

--- Website/index.php
+++ Website/index.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="stylesheet" href="css/main.css" type="text/css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="scripts/schedule.js"></script>
 
 </head>
 <body>
