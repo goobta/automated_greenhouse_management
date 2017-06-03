52d16478ce9870596527c7d7642fcbf12a49a031
Ankur Gupta <rukna1000@gmail.com>
2017-06-03 11:48:40 -0400

    Making the code Kloman-proof

--- Website/php/Blocks/Schedule/updateEvent.php
+++ Website/php/Blocks/Schedule/updateEvent.php
@@ -10,7 +10,12 @@
         $id = $_GET['id'];
         $bedNumber = $_GET['bed_number'];
         $weekDay = $_GET['weekday'];
-        $duration = Schedule::minutesToSeconds($_GET['durMinutes'], $_GET['durSeconds']);
+
+        $minutes = (isset($_POST['durMinutes']) ? $_POST['durMinutes'] : 0);
+        $seconds = (isset($_POST['durSeconds']) ? $_POST['durSeconds'] : 0);
+
+        $duration = Schedule::minutesToSeconds($minutes, $seconds);
+
         $startTime = $_GET['startTime'];
 
         Schedule::editEvent($id, $bedNumber, $weekDay, $startTime, $duration);
