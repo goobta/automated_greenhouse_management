131761970ffc92338caaa554e4cb81ac39ca74c9
Ankur Gupta <rukna1000@gmail.com>
2017-06-03 19:56:01 -0400

    Combination of me being dump and absolute paths... cronjob now works

--- Website/php/Blocks/ArduinoCommunicator/eventDameon.php
+++ Website/php/Blocks/ArduinoCommunicator/eventDameon.php
@@ -4,25 +4,22 @@
  * Date: 5/30/17
  */
 
-print getcwd();
-
 include (dirname(__FILE__) . "/../../Utility/config.php");
 include (dirname(__FILE__) . "/../Schedule/schedule.php");
+
+date_default_timezone_set("America/New_York");
 
 $connection = Config::createConnection();
 
 $currentDay = Schedule::dayToNumber(date("l"));
 $currentTime = date("H:i") . ":00";
 
-$query = "SELECT Bed, Duration FROM " . Config::$dbName . "." . Config::$dbTableName . "WHERE WeekDay LIKE " . $currentDay . " AND StartTime LIKE '" . $currentTime . "'";
-
-print $query;
-
+$query = "SELECT Bed, Duration FROM " . Config::$dbName . "." . Config::$dbTableName . " WHERE WeekDay LIKE " . $currentDay . " AND StartTime LIKE '" . $currentTime . "'";
 $result = $connection->query($query);
 
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        exec("sudo /usr/bin/python " . dirname(__FILE__) . "/var/www/html/python/Water.py " . $row["Bed"] . " " . $row["Duration"] . " > /dev/null 2>/dev/null &");
+        exec("sudo /usr/bin/python  /var/www/html/python/Water.py " . $row["Bed"] . " " . $row["Duration"] . " > /dev/null 2>/dev/null &");
 
         sleep(1);
     }
---
+++ Website/php/Blocks/Schedule/test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agupta
+ * Date: 6/3/2017
+ * Time: 11:52 AM
+ */
+
+date_default_timezone_set("America/New_York");
+
+print (date("H:i") . ":00");
